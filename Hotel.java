c261ec559fbe887f9532394436aab14dc14955a3
mantita <65884385+6010613427@users.noreply.github.com>
2021-05-10 17:35:45 +0700

    update hotel and Hotelservice

--- Hotel.java
+++ Hotel.java
@@ -12,28 +12,29 @@ import java.util.Random;
 public class Hotel{
 
 
-	Random generator = new Random();
+//	Random generator = new Random();
 	
-	public void place() {
+	public String place(String hotelplace) {
 		
 //		int attackDamage = generator.nextInt(10) + 1;
-                String hotelplace = "Bali"; 
+//                String hotelplace = "Bali"; 
 		
-		System.out.println("Thank for choose " + hotelplace + " we are welcome!");
-		
+		System.out.println("Thank for choose " + hotelplace + " Hotel, we are welcome!");
+		return  hotelplace;
 	}
 
 	public void roomleft() {
 		
-		int roomLeft = generator.nextInt(100) + 1;
+		int roomLeft = 20;
 		
 		System.out.println("Available Rooms " + roomLeft + " rooms");
 		
 	}
 
-	public void bookName(String customerName) {
+	public String bookName(String customerName) {
 		
 		System.out.println("Customer name: " + customerName);
+                return customerName;
 		
 	}
 	
--- HotelService.java
+++ HotelService.java
@@ -1,44 +1,70 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author HP
  */
 public class HotelService {
-    	public static void main(String[] args){
-		
-		HotelBooking bookhotel = new HotelBooking();
-		
-		Hotel eachhotel = new Hotel();
-		
-		HotelTarget robotAdapter = new HotelAdapter(eachhotel);
-		
-		System.out.println("The Hotel");
-		
-		eachhotel.bookName("Paul");
-		eachhotel.roomleft();
-		eachhotel.place();
-		System.out.println();
-		
-		System.out.println("The Hotel Service");
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Welcome to Hotel-Service: ");
+        System.out.println("Please select number => 1 Japan Hotel, 2 China Hotel, 3 Hongkong Hotel, 4 Bali Hotel, 5 Canada Hotel");
+        System.out.print("Which\'s hotel do you want to booking: ");
+        char country = reader.next().charAt(0);
+        String nameHotel = "";
+        switch (country) {
+            case '1':
+                nameHotel = "Japan Hotel";
+                break;
+            case '2':
+                nameHotel = "China";
+                break;
+            case '3':
+                nameHotel = "Hongkong";
+                break;
+            case '4':
+                nameHotel = "Bali";
+                break;
+            case '5':
+                nameHotel = "Canada";
+                break;
+            default:
+                System.out.println("Please try again.");
+        }
+        System.out.println("Welcome to " + nameHotel + " Hotel");
+
+        //HotelBooking bookhotel = new HotelBooking();
+        Hotel eachhotel = new Hotel();
+
+        //HotelTarget hotelAdapter = new HotelAdapter(eachhotel);
+//		System.out.println("The Hotel");
+        Scanner customername = new Scanner(System.in);
+        System.out.print("Please fill in your name: ");
+        String name = customername.nextLine();
+        eachhotel.bookName(name);
+        eachhotel.roomleft();
+        eachhotel.place(nameHotel);
+        System.out.println();
+
+        /*System.out.println("The Hotel Service");
 		
 		bookhotel.bookroom("Paul");
 		bookhotel.availableRooms();
 		bookhotel.hotelPlace();
 		System.out.println();
 		
-		System.out.println("The Hotel with Adapter");
+		/*System.out.println("The Hotel with Adapter");
 		
-		robotAdapter.bookroom("Paul");
-		robotAdapter.availableRooms();
-		robotAdapter.hotelPlace();
-
-                
-                
-
-	}
+		hotelAdapter.bookroom("Paul");
+		hotelAdapter.availableRooms();
+		hotelAdapter.hotelPlace();*/
+    }
 }
