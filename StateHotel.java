85c2dfbfed30ab9f44681a7807888698d5e73957
mantita <65884385+6010613427@users.noreply.github.com>
2021-06-06 20:29:15 +0700

    update state hotel

--- StateHotel.java
+++ StateHotel.java
@@ -1,13 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+/**
+ *
+ * @author HP
+ */
 import java.util.Scanner;
-
+import javax.swing.JOptionPane;
 
 public class StateHotel {
-    
+
     private String nameHotel;
-    
+
     public String StateNameHotel(String numCountry) {
-        
+
         switch (numCountry) {
             case "1":
                 nameHotel = "Japan Hotel";
@@ -27,36 +36,45 @@ public class StateHotel {
             default:
                 System.out.println("Please try again.");
         }
-        
+
         return nameHotel;
     }
-    
-    public void StateFood (String choice) {
-        
+
+    public void StateFood(String choice) {
+
         int l;
         HotelKeeper keeper = new HotelKeeper();
         Scanner sc = new Scanner(System.in);
-        
+
         switch (choice) {
-                case "N":
-                    keeper.getNonVegMenu();
-                    System.out.print("Select your food from number : ");
-                    l = sc.nextInt();
-                    keeper.getNonVegFood(l, nameHotel);
-                    break;
-                case "V":
-                    keeper.getVegMenu();
-                    System.out.print("Select your food from number : ");
-                    l = sc.nextInt();
-                    keeper.getVegFood(l, nameHotel);
-                    break;
-                case "B":
-                    keeper.getBothMenu();
-                    System.out.print("Select your food from number : ");
-                    l = sc.nextInt();
-                    keeper.getBothFood(l, nameHotel);
-                    break;
-            }
+            case "N":
+                keeper.getNonVegMenu();
+
+                //System.out.print("Select your food from number : ");
+                //l = sc.nextInt();
+                String foodNum;
+                foodNum = JOptionPane.showInputDialog("Select your food from number : ");
+                l = Integer.parseInt(foodNum);
+                keeper.getNonVegFood(l, nameHotel);
+                break;
+            case "V":
+                keeper.getVegMenu();
+
+                foodNum = JOptionPane.showInputDialog("Select your food from number : ");
+                l = Integer.parseInt(foodNum);
+                //System.out.print("Select your food from number : ");
+                //l = sc.nextInt();
+                keeper.getVegFood(l, nameHotel);
+                break;
+            case "B":
+                keeper.getBothMenu();
+                foodNum = JOptionPane.showInputDialog("Select your food from number : ");
+                l = Integer.parseInt(foodNum);
+                //System.out.print("Select your food from number : ");
+                //l = sc.nextInt();
+                keeper.getBothFood(l, nameHotel);
+                break;
+        }
     }
-    
+
 }
